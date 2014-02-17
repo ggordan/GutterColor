9c0739b758cac6d1c38abe2ce0bd979f9caf2b1b
Gordan Grasarevic <me@ggordan.com>
2014-02-17 21:23:09 +0000

    Add comments to settings.

--- settings.py
+++ settings.py
@@ -1,4 +1,7 @@
 from os.path import dirname, realpath, join
 
+# The path where the color cache is stored
 CACHE_PATH = join(dirname(realpath(__file__)), '.cache')
+
+# The path where the icons are stored
 ICON_PATH = join(dirname(realpath(__file__)), 'icons')
