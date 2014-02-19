98fbc7499e50367d7b57cbeb1a08e83699658e73
Gordan Grasarevic <me@ggordan.com>
2014-02-19 00:01:38 +0000

    Only Run for certain syntax.

--- gutter_color.py
+++ gutter_color.py
@@ -1,4 +1,5 @@
 from sublime_plugin import EventListener
+import GutterColor.settings as settings
 from GutterColor.clean import Clean
 from GutterColor.file import File
 import sublime
@@ -7,11 +8,15 @@ class GutterColorEventListener(EventListener):
 
   def on_load_async(self, view):
     '''Initialize the file by creating the regions'''
-    File(view).start()
+    syntax = view.settings().get('syntax').split('/')[-1].split('.')[0].lower()
+    if syntax in settings.SUPPORTED_SYNTAX:
+      File(view).start()
 
   def on_pre_save_async(self, view):
     '''Update the regions in the view'''
-    File(view, 'update').start()
+    syntax = view.settings().get('syntax').split('/')[-1].split('.')[0].lower()
+    if syntax in settings.SUPPORTED_SYNTAX:
+      File(view, 'update').start()
 
   def on_pre_close(self, view):
     """Ensure that all the assets are deleted, including for files which are
@@ -19,6 +24,8 @@ class GutterColorEventListener(EventListener):
     N.B. This needs to be a on_pre_close() since view.window() isn't accessible
     on on_close().
     """
-    views = map(lambda x: x.views(), sublime.windows())
-    open_files = list(map((lambda x: x.buffer_id()), [x for y in views for x in y]))
-    Clean(open_files).start()
+    syntax = view.settings().get('syntax').split('/')[-1].split('.')[0].lower()
+    if syntax in settings.SUPPORTED_SYNTAX:
+      views = map(lambda x: x.views(), sublime.windows())
+      open_files = list(map((lambda x: x.buffer_id()), [x for y in views for x in y]))
+      Clean(open_files).start()
