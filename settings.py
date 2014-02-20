c31ff79a5a74c23b1849c159d91a916822c6b873
Gordan Grasarevic <me@ggordan.com>
2014-02-20 22:49:52 +0000

    Remove stuff.

--- settings.py
+++
@@ -1,7 +0,0 @@
-from os.path import dirname, realpath, join
-
-# The path where the color cache is stored
-CACHE_PATH = join(dirname(realpath(__file__)), '.cache')
-
-# The path where the icons are stored
-ICON_PATH = join(dirname(realpath(__file__)), 'icons')
