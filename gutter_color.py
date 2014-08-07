dcddd6dde8b566fb221db1b7812315d9b7511041
tdhsmith <contact@tdhsmith.com>
2014-08-06 19:51:13 -0500

    Option to auto-fix icon shading in light themes
    Added functions that load, modify, save, and set color scheme files to counteract an issue in which color schemes with light backgrounds automatically darken PNGs, rendering gutter colors inaccurate.
    
    Options are provided to scan & edit global user preferences, syntax-specific settings, and custom files (for users with other plugins affecting the current color scheme). This process only modifies the highest priority file that has set the current scheme, in the order custom, syntax, global, respectively (the `load_settings` command will automatically check defaults for each if no User version is set).
    
    If enabled (defaults to off), the process will be applied to all views
    (with supported syntaxes) on plugin load, and to any such view when
    gaining focus. The process can also be triggered by a user command (which
    will force the scheme to be regenerated even if it already exists, and
    ignore user disabling of global or syntax file scan).
    
    Modified schemes are saved directly to the Gutter Color package directory
    with a special suffix. Although this might clutter the package, it simplifies the color scheme menu.

--- gutter_color.py
+++ gutter_color.py
@@ -1,6 +1,6 @@
 from .file import File
-from sublime_plugin import EventListener, WindowCommand
-from sublime import load_settings
+from sublime_plugin import EventListener, WindowCommand, TextCommand
+from sublime import load_settings, save_settings, load_resource, packages_path
 
 def clear_cache(force = False):
   """
@@ -26,6 +26,7 @@ def clear_cache(force = False):
 
 def plugin_loaded():
   clear_cache()
+  fix_schemes_in_windows()
 
 class GutterColorClearCacheCommand(WindowCommand):
   def run(self):
@@ -37,6 +38,7 @@ class GutterColorEventListener(EventListener):
   def on_activated_async(self, view):
     """Scan file when it gets focus"""
     if syntax(view) in settings().get('supported_syntax'):
+      fix_scheme_in_view(view)
       File(view)
 
   def on_modified(self, view):
@@ -65,3 +67,99 @@ def current_directory(full=False):
     return dirname(realpath(__file__))
   else:
     return split(dirname(realpath(__file__)))[1]
+
+def fix_schemes_in_windows():
+  """Change color schemes for all current views in the supported syntax list"""
+  from sublime import windows
+  for window in windows():
+    for view in window.views():
+      if syntax(view) in settings().get('supported_syntax'):
+        fix_scheme_in_view(view)
+
+def fix_scheme_in_view(view, regenerate=False, ignore_flags=False):
+  """Change color scheme in settings relevant to current view"""
+  fix_flag = settings().get("fix_color_schemes", False)
+  (fix_syntax, fix_global, fix_custom) = (False, False, False)
+  custom_files = []
+  if fix_flag == True:
+    (fix_syntax, fix_global) = (True, True)
+  elif isinstance(fix_flag, list):
+    for label in fix_flag:
+      if label in ("syntax", "syntax-specific"):
+        fix_syntax = True
+      if label in ("user", "global", "preferences"):
+        fix_global = True
+      if ".sublime-settings" in label:
+        fix_custom = True
+        custom_files.append(label)
+  elif ignore_flags:
+    pass # otherwise we might quit when we want to force a check contrary to user prefs
+  else:
+    return # setting is false, nonexistant, or malformed, so exit
+
+  current_scheme = view.settings().get("color_scheme")
+  modified_marker = ".gcfix."
+  if modified_marker in current_scheme:
+    if regenerate:
+      new_scheme = current_scheme
+    else:
+      return # this view already has a fixed scheme and we aren't regenerating, so exit
+  else:
+    new_scheme =  "Packages/"+current_directory()+"/"+current_scheme.split("/")[-1].split(".")[0]+\
+                  modified_marker + current_scheme.split(".")[-1]
+
+  if fix_custom:
+    for custom_filename in custom_files:
+      if fix_scheme_in_settings(custom_filename, current_scheme, new_scheme):
+        return
+  if fix_syntax or ignore_flags:
+    syntax_filename = view.settings().get('syntax').split('/')[-1].split('.')[0] + ".sublime-settings"
+    if fix_scheme_in_settings(syntax_filename, current_scheme, new_scheme):
+      return
+  if fix_global or ignore_flags:
+    if fix_scheme_in_settings("Preferences.sublime-settings", current_scheme, new_scheme):
+      return
+  print("Could not find or access the settings file where current color_scheme ("+current_scheme+") is set.")
+
+
+def fix_scheme_in_settings(settings_file,current_scheme, new_scheme, regenerate=False):
+  """Change the color scheme in the given Settings to a background-corrected one"""
+  from os.path import join, normpath, isfile
+
+  settings = load_settings(settings_file)
+  settings_scheme = settings.get("color_scheme")
+  if current_scheme == settings_scheme:
+    new_scheme_path =  join(packages_path(), normpath(new_scheme[len("Packages/"):]))
+    if isfile(new_scheme_path) and not regenerate:
+      settings.set("color_scheme", new_scheme)
+    else:
+      generate_scheme_fix(current_scheme, new_scheme_path)
+      settings.set("color_scheme", new_scheme)
+    save_settings(settings_file)
+    return True
+  return False
+
+def generate_scheme_fix(old_scheme, new_scheme_path):
+  """Appends background-correction XML to a color scheme file"""
+  from os.path import join
+  from re import sub
+  UUID_REGEX = '[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}'
+
+  with open(join(packages_path(),current_directory(),'background_fix.xml')) as f:
+    xml = f.read()
+  scheme_data = load_resource(old_scheme) # only valid for ST3 API!
+
+  insertion_point = scheme_data.rfind("</array>")
+  new_scheme_data = scheme_data[:insertion_point] + xml + scheme_data[insertion_point:]
+
+  def uuid_gen(args):
+    from uuid import uuid4
+    return str(uuid4())
+  new_scheme_data = sub(UUID_REGEX, uuid_gen, new_scheme_data)
+
+  with open(new_scheme_path, "wb") as f:
+    f.write(new_scheme_data.encode("utf-8"))
+
+class GutterColorFixCurrentScheme(TextCommand):
+  def run(self, args):
+    fix_scheme_in_view(self.view, True, True)
